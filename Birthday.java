10d3003ffb4cb42e5652b1da7d941a4498db8fbc
hoangk2001fn <60439813+hoangk2001fn@users.noreply.github.com>
2020-03-02 11:12:14 -0500

    Add files via upload

--- Birthday.java
+++ Birthday.java
@@ -5,14 +5,22 @@ import java.text.NumberFormat;
 public class Birthday {
 
 	public static void main(String[] args) {
+		//String variables to pass user inputs into Toy Class
 		String childName = null, toyChoice = null, childAge, repeat = null, cardAndBalloon, moreToy;
+		//a boolean to determine if the user wants to continue after wrong information or add another gift
 		boolean ifContinue = true;
+		//Programmer name
 		String programmerName = "Hoang Nguyen";
         Toy kidToyChoice = new Toy();
-        int childAgeNum, orderNum;
+        //an integer to convert string user input for the age of the child
+        int childAgeNum;
+        //Generate and display order number
+        Random rand = new Random();
+        int orderNum = rand.nextInt(100000)+10000;
+        //A variable to keep track of the total cost of user
         double total = 0;
+        //Display the total cost of user in dollar currency format
         NumberFormat totalCost = NumberFormat.getCurrencyInstance();
-		Random rand = new Random();
 		JOptionPane.showMessageDialog(null, "Welcome to the Toy Company to choose gifts for young children");
 		while (ifContinue == true){
 			kidToyChoice.addCost(kidToyChoice.getCost());
@@ -22,6 +30,7 @@ public class Birthday {
 			kidToyChoice.setAge(childAgeNum);
 			toyChoice = JOptionPane.showInputDialog("Choose a toy: a plushie, blocks, or a book");
 			kidToyChoice.setToy(toyChoice);
+			//If the user inputs for age or toy choice doesn't match up according to menu
 			if (kidToyChoice.ageOK() == false) {
 				repeat = JOptionPane.showInputDialog("The age for the toy is not appropriate\n" + "Would you like to enter again or not?");
 				if (repeat.equalsIgnoreCase("no")) {
@@ -44,8 +53,7 @@ public class Birthday {
 			}
 
 		}
-		orderNum = rand.nextInt(99999);
-
+ 
 		System.out.println("The total cost of your order is " + totalCost.format(total) + ". Order number is " + orderNum);
 		System.out.println("Programmer: " + programmerName);
         System.exit(0);
